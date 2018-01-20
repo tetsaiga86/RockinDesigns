abc994045f811410bbf9389011e48e2027b3b1db
R. Kyle Kennedy <tetsiaga86@gmail.com>
2018-01-19 19:37:59 -0800

    db for clients wiring

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: 'spa#index'
 
-  get '/:spa_route' => 'spa#spa_route'
 
   # TODO: put in admin namespace and restrict access
   #client-Admin
@@ -10,4 +9,6 @@ Rails.application.routes.draw do
 
   #client
   post '/client' => 'client#new'
+
+  get '/:spa_route' => 'spa#spa_route'
 end
